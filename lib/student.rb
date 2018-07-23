be72e713b586c8555553af150e1b288b8f187934
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-23 18:29:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,8 @@ class Student
   def self.first_student_in_grade_10
     self.all.select{|student| student.grade == '10'}.first
   end
+
+  def self.all_students_in_grade_X(x)
+    grade_x_students = self.all.select{|student| student.grade == 'x'}
+  end
 end
