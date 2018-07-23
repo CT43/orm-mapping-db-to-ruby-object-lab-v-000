52bd0011edd6929fd03b52b622fac52f9e45504e
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-23 18:27:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
 
   def self.first_X_students_in_grade_10(x)
     grade_10_students = self.all.select{|student| student.grade == '10'}
-    grade_10_students.slice(0,x-1)
+    grade_10_students.slice(0,x)
   end
 
   def self.first_student_in_grade_10
